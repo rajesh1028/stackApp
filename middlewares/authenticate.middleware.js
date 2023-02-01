73c2b82c01930b4c4aa25469efbb20766985495b
rajesh-fw22-0030 <rajeshkaruppusamy@gmail.com>
2023-02-02 00:08:42 +0530

    BE completed

--- middlewares/authenticate.middleware.js
+++ middlewares/authenticate.middleware.js
@@ -25,10 +25,6 @@ const authenticate = (req, res, next) => {
         })
     }
 
-
-
-
-
     // const token = req.headers.authorization
     // if (token) {
     //     const decoded = jwt.verify(token, process.env.key)
